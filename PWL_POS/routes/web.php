e2c612cc526b0c41b4477fe45215d43c8ffece9b
nailahsalsabila27 <nailahslsb@gmail.com>
2026-03-02 10:37:30 +0700

    jobsheet3_praktikum4

--- PWL_POS/routes/web.php
+++ PWL_POS/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/level', [LevelController::class, 'index']);
+

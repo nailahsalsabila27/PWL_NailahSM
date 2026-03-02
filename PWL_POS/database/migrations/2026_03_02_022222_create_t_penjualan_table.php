5e94091464ea7cecae66b1df81ff21959e82e7ec
nailahsalsabila27 <nailahslsb@gmail.com>
2026-03-02 09:32:17 +0700

    jobsheet3_praktikum2.2

---
+++ PWL_POS/database/migrations/2026_03_02_022222_create_t_penjualan_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('t_penjualan', function (Blueprint $table) {
+            $table->id('penjualan_id');
+
+            $table->unsignedBigInteger('user_id');
+
+            $table->string('penjualan_kode', 20)->unique();
+            $table->dateTime('tanggal_penjualan');
+            $table->integer('total_harga');
+
+            $table->timestamps();
+
+            // Foreign Key
+            $table->foreign('user_id')->references('user_id')->on('m_user');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('t_penjualan');
+    }
+};

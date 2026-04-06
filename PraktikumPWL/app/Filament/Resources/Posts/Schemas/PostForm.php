723a1595173ba6de9e4119daf4e72f91c088cb30
nailahsalsabila27 <nailahslsb@gmail.com>
2026-04-06 10:15:00 +0700

    jobsheet6_praktikum2

--- PraktikumPWL/app/Filament/Resources/Posts/Schemas/PostForm.php
+++ PraktikumPWL/app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -12,33 +12,64 @@ use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\DateTimePicker;
+use Filament\Schemas\Components\Section;
+use Filament\Support\Icons\Heroicon;
+use Filament\Schemas\Components\Group;
 
 class PostForm
 {
     public static function configure(Schema $schema): Schema
-    {
-        return $schema
-            ->components([
-                //
-                TextInput::make('title')
-                    ->required()
-                    ->minLength(5),
-                TextInput::make('slug')
-                    ->required()
-                    ->unique(ignoreRecord: true),
-                Select::make('category_id')
-                    ->relationship("category", "name")
-                    ->preload()
-                    ->searchable(),
-                ColorPicker::make('color'),
-                MarkdownEditor::make('content'),
-                //RichEditor::make('content'),
-                FileUpload::make('image')
-                    ->disk('public')
-                    ->directory('post'),
-                TagsInput::make('tags'),
-                Checkbox::make('published'),
-                DateTimePicker::make('published_at'),
-            ]);
+{
+    return $schema
+        ->components([
+
+            // LEFT SIDE (2/3)
+            Section::make('Post Details')
+                ->icon('heroicon-o-document-text')
+                ->schema([
+                    TextInput::make('title')
+                        ->required()
+                        ->minLength(5),
+
+                    TextInput::make('slug')
+                        ->required()
+                        ->unique(ignoreRecord: true),
+
+                    Select::make('category_id')
+                        ->relationship('category', 'name')
+                        ->searchable(),
+
+                    ColorPicker::make('color'),
+
+                    MarkdownEditor::make('content')
+                    ->columnSpanFull(),
+                ])
+                ->columns(2)
+                ->columnSpan(8),
+
+            // RIGHT SIDE (1/3)
+            Group::make([
+
+                Section::make('Image')
+                    ->icon('heroicon-o-photo')
+                    ->schema([
+                        FileUpload::make('image')
+                            ->disk('public')
+                            ->directory('post'),
+                    ]),
+
+                Section::make('Meta')
+                    ->icon('heroicon-o-cog')
+                    ->schema([
+                        TagsInput::make('tags'),
+                        Checkbox::make('published'),
+                        DateTimePicker::make('published_at'),
+                    ]),
+
+            ])->columnSpan(4),
+
+        ])
+        ->columns(12);
     }
 }
+

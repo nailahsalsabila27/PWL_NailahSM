485321d622c4079531f9babcd6ef08d3a4587efe
nailahsalsabila27 <nailahslsb@gmail.com>
2026-04-06 11:23:54 +0700

    jobsheet6_praktikum3

--- PraktikumPWL/app/Filament/Resources/Posts/Schemas/PostForm.php
+++ PraktikumPWL/app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -29,14 +29,24 @@ class PostForm
                 ->schema([
                     TextInput::make('title')
                         ->required()
-                        ->minLength(5),
+                        ->rules(['min:5'])
+                        ->validationMessages([
+                            'required' => 'Title wajib diisi',
+                            'min' => 'Title minimal 5 karakter',
+                        ]),
 
                     TextInput::make('slug')
                         ->required()
-                        ->unique(ignoreRecord: true),
-
+                        ->rules(['min:3'])
+                        ->unique(ignoreRecord: true)
+                        ->validationMessages([
+                            'unique' => 'Slug tidak boleh sama',
+                        ]),
+                        
                     Select::make('category_id')
                         ->relationship('category', 'name')
+                        ->required()
+                        ->preload()
                         ->searchable(),
 
                     ColorPicker::make('color'),
@@ -54,6 +64,7 @@ class PostForm
                     ->icon('heroicon-o-photo')
                     ->schema([
                         FileUpload::make('image')
+                            ->required()
                             ->disk('public')
                             ->directory('post'),
                     ]),

943acfef59b674ddfe8c8912a447e42c6777b3db
nailahsalsabila27 <nailahslsb@gmail.com>
2026-05-19 02:39:59 +0700

    jobsheet7_praktikum3

--- PraktikumPWL/app/Filament/Resources/Products/Schemas/ProductInfolist.php
+++ PraktikumPWL/app/Filament/Resources/Products/Schemas/ProductInfolist.php
@@ -6,6 +6,8 @@ use Filament\Schemas\Components\Section;
 use Filament\Infolists\Components\TextEntry;
 use Filament\Infolists\Components\ImageEntry;
 use Filament\Infolists\Components\IconEntry;
+use Filament\Schemas\Components\Tabs;
+use Filament\Schemas\Components\Tabs\Tab;
 use Filament\Schemas\Schema;
 
 class ProductInfolist
@@ -15,6 +17,69 @@ class ProductInfolist
         return $schema
             ->components([
                 //
+                Tabs::make('Product Tabs')
+                ->vertical()
+                ->tabs([
+                    Tab::make('Product Details')
+                    ->icon('heroicon-o-information-circle')
+                    ->schema([
+                        TextEntry::make('name')
+                            ->label('Product Name')
+                            ->weight('bold')
+                            ->color('primary'),
+                        TextEntry::make('id')
+                            ->label('Product ID'),
+                        TextEntry::make('sku')
+                            ->label('Product SKU')
+                            ->badge()
+                            ->color('warning'),
+                        TextEntry::make('description')
+                            ->label('Product Description'),
+                        TextEntry::make('created_at')
+                            ->label('Product Creation Date')
+                            ->date('d M Y')
+                            ->color('info'),
+                    ]),
+                    Tab::make('Product Price & Stock')
+                    ->icon('heroicon-o-currency-dollar')
+                    ->badge(fn ($record) => $record->stock)
+                    ->badgeColor(fn ($record) =>
+                        $record->stock > 10 ? 'success' :
+                        ($record->stock > 0 ? 'warning' : 'danger')
+                    )
+                    ->schema([
+                        TextEntry::make('price')
+                            ->label('Product Price')
+                            ->weight('bold')
+                            ->color('primary')
+                            ->icon('heroicon-o-currency-dollar'),
+                        TextEntry::make('stock')
+                            ->label('Product Stock'),
+                    ]),
+                    Tab::make('Image & Status')
+                    ->icon('heroicon-o-photo')
+                    ->schema([
+                        ImageEntry::make('image')
+                            ->label('Product Image')
+                            ->disk('public'),
+                        TextEntry::make('price')
+                            ->label('Product Price')
+                            ->weight('bold')
+                            ->color('primary')
+                            ->icon('heroicon-o-currency-dollar'),
+                        TextEntry::make('stock')
+                            ->label('Product Stock')
+                            ->weight('bold')
+                            ->color('primary'),
+                        IconEntry::make('is_active')
+                            ->label('Is Active?')
+                            ->boolean(),
+                        IconEntry::make('is_featured')
+                            ->label('Is Featured?')
+                            ->boolean(),
+                    ]),
+                ]) ->columnSpanFull(),
+                //->vertical(),
                 Section::make('Product Info')
                     ->description(' ')
                     ->schema([
